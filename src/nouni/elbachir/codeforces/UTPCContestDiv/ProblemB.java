75870416f358dc00696084b3d263b01a2f99387d
NE <nouni.elbachir@gmail.com>
2020-11-10 17:43:08 +0100

    fix bug

--- src/nouni/elbachir/codeforces/UTPCContestDiv/ProblemB.java
+++ src/nouni/elbachir/codeforces/UTPCContestDiv/ProblemB.java
@@ -1,13 +1,18 @@
 package nouni.elbachir.codeforces.UTPCContestDiv;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 import static java.lang.System.out;
 
 public class ProblemB {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        InputStream in1 = ProblemB.class.getResourceAsStream("in-b.txt");
+        //InputStream in1 = System.in;
+        Scanner in = new Scanner(in1);
         int N = in.nextInt(), S = 0;
         in.nextLine();
         int[][] grid = new int[26][26];
@@ -26,10 +31,10 @@ public class ProblemB {
     }
 
     private static boolean surrounded(int[][] grid, int row, int col) {
-        boolean studentAtLeft = col == 0 || grid[row][col-1] == 1;
-        boolean studentAtFront = row == 0 || grid[row-1][col] == 1;
-        boolean studentAtRight = col == 25 || grid[row][col+1] == 1;
-        boolean studentAtBack = row == 25 || grid[row+1][col] == 1;
+        boolean studentAtLeft = col == 0 || grid[row][col-1] != 0;
+        boolean studentAtFront = row == 0 || grid[row-1][col] != 0;
+        boolean studentAtRight = col == 25 || grid[row][col+1] != 0;
+        boolean studentAtBack = row == 25 || grid[row+1][col] != 0;
         return studentAtLeft && studentAtRight && studentAtBack && studentAtFront;
     }
 }
---
+++ src/nouni/elbachir/codeforces/UTPCContestDiv/ProblemC.java
@@ -0,0 +1,9 @@
+package nouni.elbachir.codeforces.UTPCContestDiv;
+
+import java.util.Scanner;
+
+public class ProblemC {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
